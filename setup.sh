a41ace5dac97000a6c3218cbc07026a03af229c9
Alex Wood <awood@redhat.com>
2013-11-22 13:55:36 -0500

    Add script to make my symlinks for me.

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+link() {
+    ln -s dotfiles/$1 $HOME
+}
+
+files=(.vimrc .vim .screenrc .tmux.conf .zshrc .vij .gitconfig
+    .gitignore .git_template/ .irbrc .psqlrc)
+
+for f in ${files[@]}; do
+    link $f
+done;
