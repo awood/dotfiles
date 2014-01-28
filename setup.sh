c65a2ccaf1293bec58f4f021911ed45f7e868101
Alex Wood <awood@redhat.com>
2014-01-28 14:18:15 -0500

    Add git submodule checkout and some output.

--- setup.sh
+++ setup.sh
@@ -6,8 +6,13 @@ link() {
         echo "Cowardly refusing to overwrite $LOC/$1"
     else
         ln -s $(pwd)/dotfiles/$1 $LOC
+        echo "Linking $1"
     fi
 }
+
+pushd $(pwd)/dotfiles
+git submodule update --init --recursive
+popd
 
 files=(.vimrc .vim .screenrc .tmux.conf .zshrc .zfunc/ .gitconfig
     .gitignore .git_template/ .irbrc .psqlrc)
