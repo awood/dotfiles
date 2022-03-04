ebb7fd86c5df12c1c2afe11824c8ec15bf9cdd44
Alex Wood <awood@redhat.com>
2022-03-04 12:26:40 -0500

    Add tig zsh completion via custom oh-my-zsh plugin

--- setup.sh
+++ setup.sh
@@ -2,10 +2,10 @@
 
 link() {
     LOC=${2:-$HOME}
-    if [ -e $LOC/$1 ]; then
+    if [ -e "$LOC/$1" ]; then
         echo "Cowardly refusing to overwrite $LOC/$1"
     else
-        ln -s $(pwd)/dotfiles/$1 $LOC
+        ln -s "$(pwd)/dotfiles/$1" "$LOC"
         echo "Linking $1"
     fi
 }
@@ -23,5 +23,9 @@ for f in ${files[@]}; do
 done;
 
 if [ -d $HOME/.oh-my-zsh/ ]; then
-    link awood.zsh-theme $HOME/.oh-my-zsh/custom
+    link awood.zsh-theme $HOME/.oh-my-zsh/custom/themes
+
+    for d in $(find "$(pwd)/dotfiles/custom-zsh-plugins" -maxdepth 1 -mindepth 1 -type d -printf '%f\n'); do
+        link "custom-zsh-plugins/$d" $HOME/.oh-my-zsh/custom/plugins
+    done
 fi
