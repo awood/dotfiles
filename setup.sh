82d119bb94a153a5473b62a568b95b609507187d
Alex Wood <awood@redhat.com>
2022-03-16 13:11:03 -0400

    More colorful dircolors

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ popd
 
 files=(.vimrc .vim .screenrc .tmux.conf .zshrc .zfunc/ .gitconfig
     .gitignore .git_template/ .irbrc .psqlrc .tigrc .gdbinit .pryrc
-    .less_colors .ackrc .ideavimrc .keychainrc)
+    .less_colors .ackrc .ideavimrc .keychainrc .dircolors)
 
 for f in ${files[@]}; do
     link $f
