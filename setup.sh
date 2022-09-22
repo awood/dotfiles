a5c39f7f0b71513244957fc3b0439121b0656ff3
Alex Wood <awood@redhat.com>
2022-09-22 10:41:13 -0400

    Add dotfile for bat
    Add ability to link a conf file to a non-home directory location

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
-#! /bin/sh
+#! /bin/bash
 
 link() {
     LOC=${2:-$HOME}
     if [ -e "$LOC/$1" ]; then
         echo "Cowardly refusing to overwrite $LOC/$1"
+    # In case $2 is a file location
+    elif [ -f "$LOC" ]; then
+        echo "Cowardly refusing to overwrite $LOC"
     else
         ln -s "$(pwd)/dotfiles/$1" "$LOC"
         echo "Linking $1"
@@ -19,7 +22,14 @@ files=(.vimrc .vim .screenrc .tmux.conf .zshrc .zfunc/ .gitconfig
     .less_colors .ackrc .ideavimrc .keychainrc .dircolors)
 
 for f in ${files[@]}; do
-    link $f
+    link "$f"
+done;
+
+declare -A other_files
+other_files=( [bat.conf]="$HOME/.config/bat/config" )
+
+for f in "${!other_files[@]}"; do
+    link "$f" "${other_files[$f]}"
 done;
 
 if [ -d $HOME/.oh-my-zsh/ ]; then
@@ -30,6 +40,14 @@ if [ -d $HOME/.oh-my-zsh/ ]; then
             link "custom-zsh-plugins/$d" "$HOME/.oh-my-zsh/custom/plugins"
         else
             echo "Plugin directory for $d already exists"
+        fi
+    done
+
+    for f in $(find "$(pwd)/dotfiles/custom-zsh" -maxdepth 1 -mindepth 1 -type f -printf '%f\n'); do
+        if [ ! -d "$HOME/.oh-my-zsh/custom/$f" ]; then
+            link "custom-zsh/$f" "$HOME/.oh-my-zsh/custom/$f"
+        else
+            echo "File $f already exists in .oh-my-zsh/custom"
         fi
     done
 fi
