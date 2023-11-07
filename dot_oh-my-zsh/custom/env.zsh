b1038f8622c939d0afcaf3a1c12a224c6bad5a70
Alex Wood <awood@redhat.com>
2023-11-07 12:01:13 -0500

    Disable colors for pre-commit
    See https://pre-commit.com/#command-line-interface

--- dot_oh-my-zsh/custom/env.zsh
+++ dot_oh-my-zsh/custom/env.zsh
@@ -4,3 +4,6 @@ export LESS="-M --jump-target=5 --shift 4"
 export PROJECT_HOME="$HOME/devel"
 export WORKON_HOME="$HOME/.virtualenvs"
 export VAGRANT_DEFAULT_PROVIDER="libvirt"
+# Disable color warnings in pre-commit as pre-commit colors the background
+# rather than the text and that doesn't display nicely with my color scheme
+export PRE_COMMIT_COLOR="never"
