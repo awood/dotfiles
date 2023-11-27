966e2efc3e6fb9d2adc6b5b6e7a91c6f061c337b
Alex Wood <awood@redhat.com>
2023-11-27 10:53:54 -0500

    Remove github PR checkout fzf function
    The https://github.com/gennaro-tedesco/gh-f extension does a better job
    that this function.  The gh-f extension renders in color and handles
    markup intelligently.  Plus, it does additional things like searching
    issues, logs, etc.

--- dot_oh-my-zsh/custom/fzf.zsh
+++ dot_oh-my-zsh/custom/fzf.zsh
@@ -164,36 +164,6 @@ podtails() {
     --preview 'oc logs --follow --all-containers --tail=10000 {1}' "$@"
 }
 
-# From https://seb.jambor.dev/posts/improving-shell-workflows-with-fzf/#pull-requests
-pr-checkout() {
-    local jq_template pr_number
-
-    jq_template='"'\
-'#\(.number) - \(.title)'\
-'\t'\
-'Author: \(.user.login)\n'\
-'Created: \(.created_at)\n'\
-'Updated: \(.updated_at)\n\n'\
-'\(.body)'\
-'"'
-
-    pr_number=$(
-        gh api 'repos/:owner/:repo/pulls' |
-        jq ".[] | $jq_template" |
-        sed -e 's/"\(.*\)"/\1/' -e 's/\\t/\t/' |
-        fzf \
-          --with-nth=1 \
-          --delimiter='\t' \
-          --preview='echo -e {2}' \
-          --preview-window=top:wrap |
-        sed 's/^#\([0-9]\+\).*/\1/'
-    )
-
-    if [ -n "$pr_number" ]; then
-      gh pr checkout "$pr_number"
-    fi
-}
-
 # CTRL-G CTRL-F for Files
 # CTRL-G CTRL-B for Branches
 # CTRL-G CTRL-T for Tags
