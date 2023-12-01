da2eb1a618a94f6008ab0560af1071d473a8a822
Alex Wood <awood@redhat.com>
2023-12-01 16:13:59 -0500

    Add env vars for test containers to work with podman

--- dot_oh-my-zsh/custom/env.zsh
+++ dot_oh-my-zsh/custom/env.zsh
@@ -7,3 +7,6 @@ export VAGRANT_DEFAULT_PROVIDER="libvirt"
 # Disable color warnings in pre-commit as pre-commit colors the background
 # rather than the text and that doesn't display nicely with my color scheme
 export PRE_COMMIT_COLOR="never"
+
+export DOCKER_HOST=unix://$XDG_RUNTIME_DIR/podman/podman.sock
+export TESTCONTAINERS_RYUK_DISABLED=true
