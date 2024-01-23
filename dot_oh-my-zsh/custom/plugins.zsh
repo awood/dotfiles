ca899e2bf215506109c2e112ada4c2f63e443fd6
Alex Wood <awood@redhat.com>
2024-01-23 16:10:04 -0500

    Add fzf-marks and remove incorrect plugin loading.

--- dot_oh-my-zsh/custom/plugins.zsh
+++
@@ -1,6 +0,0 @@
-# Which plugins would you like to load?
-# Standard plugins can be found in $ZSH/plugins/
-# Custom plugins may be added to $ZSH_CUSTOM/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
-plugins=(colorize httpie poetry kubetail tig)
