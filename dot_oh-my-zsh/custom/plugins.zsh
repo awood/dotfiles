73a7fd8d461d0d8cebf05e2f02fce0261e8737b9
Alex Wood <awood@redhat.com>
2024-11-13 14:19:50 -0500

    Add plugins.zsh to control OMZsh plugin loading
    Oddly, I must have forgotten to add this file long ago.

---
+++ dot_oh-my-zsh/custom/plugins.zsh
@@ -0,0 +1,6 @@
+# Which plugins would you like to load?
+# Standard plugins can be found in $ZSH/plugins/
+# Custom plugins may be added to $ZSH_CUSTOM/plugins/
+# Example format: plugins=(rails git textmate ruby lighthouse)
+# Add wisely, as too many plugins slow down shell startup.
+plugins=(httpie poetry kubetail tig fzf-marks)
